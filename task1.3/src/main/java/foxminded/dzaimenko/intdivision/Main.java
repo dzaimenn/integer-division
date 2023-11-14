88c6ddf1ad734e59e6758ae06bbc4402305256ff
Dmytro Zaimenko <dmytro.zaimenko@gmail.com>
2023-11-14 19:32:28 +0200

    Initial commit

--- task1.3/src/main/java/foxminded/dzaimenko/intdivision/Main.java
+++ task1.3/src/main/java/foxminded/dzaimenko/intdivision/Main.java
@@ -2,6 +2,6 @@ package foxminded.dzaimenko.intdivision;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Initial commit");
+        System.out.println("Hello world!");
     }
 }
